85bca0d3dbc21e2c090ae1e0a50b4cc40f283a7c
valma <muceravn@gmail.com>
2022-05-31 20:32:28 +0300

    commit tree a5dc0c6c1ba4e1b4e82233e942d3f7b36784a49b

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+return a + b;
+}
+function subtract(a, b){
+return a -b;
+}
+function multiply(a, b){
+return a * b;
+}
+function divide(a, b){
+return a / b;
+}
+function increment(a){
+    return (a += 1);
+}
+function decrement(a){
+return (a -=1 );
+}
+function makeInt(string){
+parseInt("string", 10);
+return (string);
+}
+function preserveDecimal(string){
+parseFloat("string");
+return ("string");
+}
